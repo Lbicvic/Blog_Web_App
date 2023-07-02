9c3be19c7e31ace1914906a41ee110aff0e9c8b2
Lbicvic <leopold.bicvic@gmail.com>
2023-07-02 14:11:52 +0200

    add login form and auth context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useContext, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import axios from "axios";
 import './App.css'
+import AuthContext from "./context/AuthContext";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { setCurrentUser } = useContext(AuthContext);
 
+  if (localStorage.getItem("user")) {
+    axios.defaults.baseURL = 'http://localhost:3005';
+    axios.defaults.headers.common = {
+      authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token}`,
+    };
+  }
+
+  useEffect(() => {
+    axios
+      .get("/user/getUser", {
+        withCredentials: true,
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((response) => {
+        setCurrentUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  });
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginForm />} ></Route>
+        </Routes>
+      </Router>
+    </>
     </>
   )
 }
---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useRef, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import { useContext } from "react";
+
+const LoginForm = () => {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const navigate = useNavigate();
+  const { setCurrentUser } = useContext(AuthContext);
+  const [error, setError] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    axios
+      .post("/user/login", JSON.stringify(user), {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((response) => {
+        setError("");
+        setCurrentUser(response.data.data);
+        localStorage.setItem("user", JSON.stringify(response.data.token));
+        navigate("/homepage");
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        setError(error.response.data.error);
+      });
+  }
+
+  return (
+    <section className="login-form">
+      <div className="login-form__wrapper">
+        <h2>Login</h2>
+        {error && <div className="error"> {error} </div>}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            ref={usernameRef}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="on"
+            ref={passwordRef}
+            required
+          />
+          <button type="submit">Log in</button>
+          <p>
+            Need an account?{" "}
+            <Link to="/register" className="form__link">
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default LoginForm;
